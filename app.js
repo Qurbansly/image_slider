71234b61d0231666469ac461f5265855bf91edb2
Gurban Suleymanov <gurbansly@gmail.com>
2021-11-27 20:16:52 +0400

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const carouselSlide = document.querySelector(".carousel-slide");
 const CarouselImage = document.querySelectorAll(".carousel-slide img");
 
-const prevBtn = document.querySelector("#prevbtn");
-const nectBtn = document.querySelector("#nextbtn");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
 
 let index = 1;
 
 const size = CarouselImage[0].clientWidth;
 
 
-carouselSlide.style.transform=''
+carouselSlide.style.transform='translateX('+(-size*index)+'px)';
+
+nextBtn.addEventListener('click', ()=>{
+
+    if(index >= CarouselImage.length - 1) return;
+    carouselSlide.style.transition="transform 0.2s ease-in-out";
+    index++;
+
+    carouselSlide.style.transform = 'translateX('+(-size*index)+'px)';
+});
+
+
+prevBtn.addEventListener('click', ()=>{
+    if(index <= 0) return;
+    carouselSlide.style.transition="transform 0.2s ease-in-out";
+    index--;
+
+    carouselSlide.style.transform = 'translateX('+(-size*index)+'px)';
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if(CarouselImage[index].id ==='lastimage'){
+        carouselSlide.style.transition = 'none';
+        index = CarouselImage.length-2;
+        carouselSlide.style.transform='translateX('+(-size*index)+'px)';
+    }
+    if(CarouselImage[index].id ==='firstimage'){
+        carouselSlide.style.transition = 'none';
+        index = CarouselImage.length-index;
+        carouselSlide.style.transform='translateX('+(-size*index)+'px)';
+    }
+});
